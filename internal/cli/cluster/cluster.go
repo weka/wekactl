86d44ca3f5bfb59ac60d9cf5f185316331377bc0
Assaf Giladi <assaf@weka.io>
2020-12-10 11:22:11 +0200

    feat: add cli skeleton

---
+++ internal/cli/cluster/cluster.go
@@ -0,0 +1,18 @@
+package cluster
+
+import (
+	"github.com/spf13/cobra"
+	"log"
+)
+
+var Name string
+var Cluster = &cobra.Command{
+	Use:   "cluster [command] [flags]",
+	Short: "Cluster operations",
+	Run: func(c *cobra.Command, _ []string) {
+		if err := c.Help(); err != nil {
+			log.Printf("ignoring cobra error %q", err.Error())
+		}
+	},
+	SilenceUsage: true,
+}
