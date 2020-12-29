361076bdba2bc8b57fa6640a945b03d52811c449
Assaf Giladi <assaf@weka.io>
2020-12-29 20:05:33 +0200

    refactor: design change: use struct instead of map to hole instances info

--- internal/aws/cluster/import.go
+++ internal/aws/cluster/import.go
@@ -12,8 +12,10 @@ import (
 	"wekactl/internal/aws/common"
 )
 
-type InstancesMap map[string][]*ec2.Instance
-type AutoScalingGroupMap map[string]string
+type StackInstances struct {
+	backends []*ec2.Instance
+	clients  []*ec2.Instance
+}
 
 var Roles = []string{
 	"backends",
@@ -38,7 +40,7 @@ func getClusterInstances(region, stackName string) []*string {
 	return instancesIds
 }
 
-func getInstancesInfo(region, stackName string) InstancesMap {
+func getInstancesInfo(region, stackName string) StackInstances {
 	sess := common.NewSession(region)
 	svc := ec2.New(sess)
 	input := &ec2.DescribeInstancesInput{
@@ -49,24 +51,19 @@ func getInstancesInfo(region, stackName string) InstancesMap {
 		log.Fatal().Err(err)
 	}
 
-	clusterInstances := make(InstancesMap)
-	var backends []*ec2.Instance
-	var clients []*ec2.Instance
+	stackInstances := StackInstances{}
 
 	for _, reservation := range result.Reservations {
 		instance := reservation.Instances[0]
 		arn := *instance.IamInstanceProfile.Arn
 		if strings.Contains(arn, "InstanceProfileBackend") {
-			backends = append(backends, instance)
+			stackInstances.backends = append(stackInstances.backends, instance)
 		} else if strings.Contains(arn, "InstanceProfileClient") {
-			clients = append(clients, instance)
+			stackInstances.clients = append(stackInstances.clients, instance)
 		}
 
 	}
-
-	clusterInstances["backends"] = backends
-	clusterInstances["clients"] = clients
-	return clusterInstances
+	return stackInstances
 }
 
 func getInstanceSecurityGroupsId(instance *ec2.Instance) []*string {
@@ -109,37 +106,34 @@ func createLaunchTemplate(region, stackName, role string, instance *ec2.Instance
 	return launchTemplateName
 }
 
-func createAutoScalingGroup(region, stackName string, clusterInstances InstancesMap) AutoScalingGroupMap {
-	autoScalingGroupsNames := make(AutoScalingGroupMap)
-	for _, role := range Roles {
-		if len(clusterInstances[role]) > 0 {
-			launchTemplateName := createLaunchTemplate(region, stackName, role, clusterInstances[role][0])
-			instancesNumber := int64(len(clusterInstances[role]))
-			sess := common.NewSession(region)
-			svc := autoscaling.New(sess)
-			u := uuid.New().String()
-			name := "weka-" + stackName + "-" + role + "-" + u
-			input := &autoscaling.CreateAutoScalingGroupInput{
-				AutoScalingGroupName: aws.String(name),
-				LaunchTemplate: &autoscaling.LaunchTemplateSpecification{
-					LaunchTemplateName: aws.String(launchTemplateName),
-					Version: aws.String("1"),
-				},
-				MinSize:         aws.Int64(0),
-				MaxSize:         aws.Int64(instancesNumber),
-			}
-			_, err := svc.CreateAutoScalingGroup(input)
-			if err != nil {
-				log.Fatal().Err(err)
-			}
-			autoScalingGroupsNames[role] = name
-			log.Debug().Msgf("AutoScalingGroup: \"%s\" was created sucessfully!", name)
-		} else {
-			fmt.Printf("No %s where found\n", strings.Title(role))
+func createAutoScalingGroup(region, stackName, role string, roleInstances []*ec2.Instance) string {
+	if len(roleInstances) > 0 {
+		launchTemplateName := createLaunchTemplate(region, stackName, role, roleInstances[0])
+		instancesNumber := int64(len(roleInstances))
+		sess := common.NewSession(region)
+		svc := autoscaling.New(sess)
+		u := uuid.New().String()
+		name := "weka-" + stackName + "-" + role + "-" + u
+		input := &autoscaling.CreateAutoScalingGroupInput{
+			AutoScalingGroupName: aws.String(name),
+			LaunchTemplate: &autoscaling.LaunchTemplateSpecification{
+				LaunchTemplateName: aws.String(launchTemplateName),
+				Version:            aws.String("1"),
+			},
+			MinSize: aws.Int64(0),
+			MaxSize: aws.Int64(instancesNumber),
 		}
+		_, err := svc.CreateAutoScalingGroup(input)
+		if err != nil {
+			log.Fatal().Err(err)
+		}
+		log.Debug().Msgf("AutoScalingGroup: \"%s\" was created sucessfully!", name)
+		return name
+	} else {
+		fmt.Printf("No %s where found\n", strings.Title(role))
+		return ""
 	}
-
-	return autoScalingGroupsNames
+	return ""
 }
 
 func getInstancesIdsFromEc2Instance(instances []*ec2.Instance) []*string {
@@ -150,26 +144,28 @@ func getInstancesIdsFromEc2Instance(instances []*ec2.Instance) []*string {
 	return instanceIds
 }
 
-func attachInstancesToAutoScalingGroups(region string, clusterInstances InstancesMap, autoScalingGroupsNames AutoScalingGroupMap) {
+func attachInstancesToAutoScalingGroups(region string, roleInstances []*ec2.Instance, autoScalingGroupsName string) {
 	sess := common.NewSession(region)
 	svc := autoscaling.New(sess)
-	for _, role := range Roles {
-		if name, ok := autoScalingGroupsNames[role]; ok {
-			input := &autoscaling.AttachInstancesInput{
-				AutoScalingGroupName: &name,
-				InstanceIds:          getInstancesIdsFromEc2Instance(clusterInstances[role]),
-			}
-			_, err := svc.AttachInstances(input)
-			if err != nil {
-				log.Fatal().Err(err)
-			}
-			log.Debug().Msgf("Attaced %d instances to %s successfully!", len(clusterInstances[role]), name)
-		}
+	input := &autoscaling.AttachInstancesInput{
+		AutoScalingGroupName: &autoScalingGroupsName,
+		InstanceIds:          getInstancesIdsFromEc2Instance(roleInstances),
 	}
+	_, err := svc.AttachInstances(input)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	log.Debug().Msgf("Attaced %d instances to %s successfully!", len(roleInstances), autoScalingGroupsName)
+
+}
+
+func importClusterRole(region, stackName, role string, roleInstances []*ec2.Instance) {
+	autoScalingGroupName := createAutoScalingGroup(region, stackName, role, roleInstances)
+	attachInstancesToAutoScalingGroups(region, roleInstances, autoScalingGroupName)
 }
 
 func ImportCluster(region, stackName string) {
-	clusterInstances := getInstancesInfo(region, stackName)
-	autoScalingGroupsNames := createAutoScalingGroup(region, stackName, clusterInstances)
-	attachInstancesToAutoScalingGroups(region, clusterInstances, autoScalingGroupsNames)
+	stackInstances := getInstancesInfo(region, stackName)
+	importClusterRole(region, stackName, "clients", stackInstances.clients)
+	importClusterRole(region, stackName, "backends", stackInstances.backends)
 }
