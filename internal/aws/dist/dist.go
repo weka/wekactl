a96ccfae6bd12874bbc79793b2b4f5f3c98c89aa
Anton Bykov <tigrawap@gmail.com>
2021-01-04 12:16:59 +0200

    Lambda generator: Helper function for receving https:// url of uploaded lambdas

--- internal/aws/dist/dist.go
+++ internal/aws/dist/dist.go
@@ -1,4 +1,28 @@
 package dist
 
+import (
+	"errors"
+	"fmt"
+)
+
 var LambdasSource = map[string]string{}
 var LambdasID string
+
+type LambdaPackage string
+
+const (
+	ScaleIn LambdaPackage = "scale_in_lambda.zip"
+	WekaCtl LambdaPackage = "wekactl.zip"
+)
+
+func GetLambdaLocation(lambdaPackage LambdaPackage, region string) (location string, err error){
+	if LambdasID == "" {
+		return "", errors.New("lambda ID not defined")
+	}
+	bucket, ok := LambdasSource[region]
+	if !ok {
+		return "", errors.New(fmt.Sprintf("bucket not defined for %s", region))
+	}
+	location = fmt.Sprintf("https://s3.%s.amazonaws.com/%s/%s/%s", region, bucket, LambdasID, string(lambdaPackage))
+	return
+}
