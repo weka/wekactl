7060f6e9ff38f7c3205d09cbb3e9c859ae57a089
Anton Bykov <tigrawap@gmail.com>
2021-01-25 19:58:56 +0200

    force-disable CGO to prevent accidentally dynamicaly linked binaries

--- scripts/build_lambdas.sh
+++ scripts/build_lambdas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 go build -o tmp/upload/lambdas-bin cmd/wekactl-aws-lambdas/*.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o tmp/upload/lambdas-bin cmd/wekactl-aws-lambdas/*.go
 cd tmp/upload || exit 1
 zip wekactl-aws-lambdas.zip lambdas-bin
 rm lambdas-bin
--- scripts/build_wekactl.sh
+++ scripts/build_wekactl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 go build -o tmp/upload/wekactl_linux_amd64 cmd/wekactl/*.go
-GOOS=darwin GOARCH=amd64 go build -o tmp/upload/wekactl_darwin_amd64 cmd/wekactl/*.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o tmp/upload/wekactl_linux_amd64 cmd/wekactl/*.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o tmp/upload/wekactl_darwin_amd64 cmd/wekactl/*.go
