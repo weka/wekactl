7a9d3e1bf19af9a4b5cf161bd6bd0c58f77e0ffc
Assaf Giladi <assaf@weka.io>
2021-03-01 17:17:34 +0200

    refactor: on cluster update, get maxSize from ASG if possible

--- internal/aws/cluster/update.go
+++ internal/aws/cluster/update.go
@@ -1,6 +1,7 @@
 package cluster
 
 import (
+	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"wekactl/internal/aws/common"
 	"wekactl/internal/cluster"
@@ -8,11 +9,11 @@ import (
 )
 
 func fetchClusterLaunchTemplateParams(clusterName cluster.ClusterName, name common.HostGroupName) (hostGroupParams common.HostGroupParams, err error) {
-	launchTemplateName := common.GenerateResourceName(clusterName, name)
+	resourceName := common.GenerateResourceName(clusterName, name)
 	svc := connectors.GetAWSSession().EC2
 
 	launchTemplateVersionsOutput, err := svc.DescribeLaunchTemplateVersions(&ec2.DescribeLaunchTemplateVersionsInput{
-		LaunchTemplateName: &launchTemplateName,
+		LaunchTemplateName: &resourceName,
 	})
 
 	if err != nil {
@@ -20,6 +21,17 @@ func fetchClusterLaunchTemplateParams(clusterName cluster.ClusterName, name comm
 	}
 
 	launchTemplateData := launchTemplateVersionsOutput.LaunchTemplateVersions[0].LaunchTemplateData
+
+	maxSize := int64(1000)
+	svcAsg := connectors.GetAWSSession().ASG
+	asgOutput, err := svcAsg.DescribeAutoScalingGroups(
+		&autoscaling.DescribeAutoScalingGroupsInput{
+			AutoScalingGroupNames: []*string{&resourceName},
+		},
+	)
+	if err == nil && len(asgOutput.AutoScalingGroups) > 0 {
+		maxSize = *asgOutput.AutoScalingGroups[0].MaxSize
+	}
 
 	hostGroupParams = common.HostGroupParams{
 		SecurityGroupsIds: launchTemplateData.SecurityGroupIds,
@@ -31,9 +43,7 @@ func fetchClusterLaunchTemplateParams(clusterName cluster.ClusterName, name comm
 		VolumeName:        *launchTemplateData.BlockDeviceMappings[0].DeviceName,
 		VolumeType:        *launchTemplateData.BlockDeviceMappings[0].Ebs.VolumeType,
 		VolumeSize:        *launchTemplateData.BlockDeviceMappings[0].Ebs.VolumeSize,
-		//TODO: MaxSize populated differently on-import and on-update
-		//Here if possible - set what is set on autoscaling group
-		//If ASG does not exist - set 1000
+		MaxSize:           maxSize,
 	}
 
 	return
