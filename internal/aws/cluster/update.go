8fb2c55c8d238bf64e0cf5dad91bfcd87a3a1086
Assaf Giladi <assaf@weka.io>
2021-03-04 13:24:40 +0200

    refactor: expose UpdateCluster to cli use

--- internal/aws/cluster/update.go
+++ internal/aws/cluster/update.go
@@ -4,6 +4,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"wekactl/internal/aws/common"
+	"wekactl/internal/aws/db"
 	"wekactl/internal/cluster"
 	"wekactl/internal/connectors"
 )
@@ -49,11 +50,55 @@ func fetchClusterLaunchTemplateParams(clusterName cluster.ClusterName, name comm
 	return
 }
 
-func GenerateHostGroupFromLaunchTemplate(clusterName cluster.ClusterName, role common.InstanceRole, name common.HostGroupName) (hostGroup HostGroup, err error) {
+func generateHostGroupFromLaunchTemplate(clusterName cluster.ClusterName, role common.InstanceRole, name common.HostGroupName) (hostGroup HostGroup, err error) {
 	hostGroupParams, err := fetchClusterLaunchTemplateParams(clusterName, name)
 	if err != nil {
 		return
 	}
 	hostGroup = GenerateHostGroup(clusterName, hostGroupParams, role, name)
 	return
+}
+
+func generateUpdateAWSCluster(stackName string) (awsCluster AWSCluster, err error) {
+	clusterName := cluster.ClusterName(stackName)
+
+	backendsHostGroup, err := generateHostGroupFromLaunchTemplate(
+		clusterName, common.RoleBackend, "Backends")
+	if err != nil {
+		return
+	}
+
+	clientsHostGroup, err := generateHostGroupFromLaunchTemplate(
+		clusterName, common.RoleClient, "Clients")
+	if err != nil {
+		return
+	}
+
+	dynamoDb := DynamoDb{
+		ClusterName: clusterName,
+	}
+
+	awsCluster = AWSCluster{
+		Name:          clusterName,
+		DefaultParams: db.DefaultClusterParams{},
+		CFStack: Stack{
+			StackName: stackName,
+		},
+		DynamoDb: dynamoDb,
+		HostGroups: []HostGroup{
+			backendsHostGroup,
+			clientsHostGroup,
+		},
+	}
+	return
+}
+
+func UpdateCluster(stackName string) error {
+	awsCluster, err := generateUpdateAWSCluster(stackName)
+	if err != nil {
+		return err
+	}
+
+	awsCluster.Init()
+	return cluster.EnsureResource(&awsCluster)
 }
