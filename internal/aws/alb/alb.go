6d3f841c765a9d9d68bfc9410d3366bead14a050
Anton Bykov <tigrawap@gmail.com>
2021-06-22 16:19:58 +0300

    client join: comments fix

--- internal/aws/alb/alb.go
+++ internal/aws/alb/alb.go
@@ -321,8 +321,8 @@ func PrintStatelessClientsJoinScript(clusterName cluster.ClusterName, dnsAlias s
 
 	curl %s:14000/dist/v1/install | sh
 
-	FILESYSTEM_NAME=default # replace with different filesystem, if not using default one
-	MOUNT_POINT=/mnt/weka # replace with different mount point, in case default one does not fit your needs
+	FILESYSTEM_NAME=default # replace with a different filesystem at need
+	MOUNT_POINT=/mnt/weka # replace with a different mount point at need
 
 	mkdir -p $MOUNT_POINT
 	mount -t wekafs %s/"$FILESYSTEM_NAME" $MOUNT_POINT
